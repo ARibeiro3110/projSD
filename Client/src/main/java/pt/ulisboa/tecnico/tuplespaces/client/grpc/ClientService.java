96cbc7a5831a142ec04cebd2655e1a59dcdb19f8
Luis Correia <luis.v.correia@tecnico.ulisboa.pt>
2024-03-11 11:37:05 +0000

    Implement client-side put and read methods using non-blocking stubs

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
@@ -1,12 +1,13 @@
 package pt.ulisboa.tecnico.tuplespaces.client.grpc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
-import pt.ulisboa.tecnico.tuplespaces.centralized.contract.TupleSpacesGrpc;
-import pt.ulisboa.tecnico.tuplespaces.centralized.contract.TupleSpacesCentralized.*;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.TupleSpacesReplicaGrpc;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.TupleSpacesReplicaXuLiskov.*;
 import pt.ulisboa.tecnico.nameserver.contract.NameServerOuterClass.LookupRequest;
 import pt.ulisboa.tecnico.nameserver.contract.NameServerOuterClass.LookupResponse;
 import pt.ulisboa.tecnico.nameserver.contract.NameServerGrpc;
@@ -22,10 +23,11 @@ public class ClientService {
 
     private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
     private NameServerGrpc.NameServerBlockingStub nameServerStub;
-    private TupleSpacesGrpc.TupleSpacesBlockingStub tupleSpacesStub;
-    private ManagedChannel channel;
-    private String target;
+    private List<TupleSpacesReplicaGrpc.TupleSpacesReplicaStub> tupleSpacesStubs;
+    private List<ManagedChannel> channels;
     private OrderedDelayer delayer;
+    private int numServers;
+    private ResponseCollector collector;
 
     /** Helper method to print debug messages. */
     private static void debug(String debugMessage) {
@@ -35,14 +37,16 @@ public class ClientService {
 
     public ClientService(int numServers) {
         delayer = new OrderedDelayer(numServers);
+        this.numServers = numServers;
+        collector = new ResponseCollector();
 
-        target = NAME_SERVER_TARGET; // target for name server
+        channels = new ArrayList<ManagedChannel>();
+        tupleSpacesStubs = new ArrayList<TupleSpacesReplicaGrpc.TupleSpacesReplicaStub>();
+        
         this.nameServerStub = createNameServerBlockingStub(); // create blocking stub for name server
 
-        debug("Looking for server...");
-        target = findServer(); // find tuple spaces server
-
-        this.tupleSpacesStub = createTupleSpacesBlockingStub(); // create blocking stub for tuple spaces server
+        debug("Looking for servers...");
+        findServers();
     }
 
     /* This method allows the command processor to set the request delay assigned to a given server */
@@ -58,15 +62,16 @@ public class ClientService {
     }
 
     private NameServerGrpc.NameServerBlockingStub createNameServerBlockingStub() {
-        debug("Creating NameServerBlockingStub for target " + target + "...");
-        this.channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        debug("Creating NameServerBlockingStub for target " + NAME_SERVER_TARGET + "...");
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(NAME_SERVER_TARGET).usePlaintext().build();
         return NameServerGrpc.newBlockingStub(channel);
     }
 
-    private TupleSpacesGrpc.TupleSpacesBlockingStub createTupleSpacesBlockingStub() {
+    private void createTupleSpacesStub(String target) {
         debug("Creating TupleSpacesBlockingStub for target " + target + "...");
-        this.channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
-        return TupleSpacesGrpc.newBlockingStub(channel);
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        channels.add(channel);
+        tupleSpacesStubs.add(TupleSpacesReplicaGrpc.newStub(channel));
     }
 
     public List<String> lookup(String name, String qualifier) {
@@ -88,7 +93,7 @@ public class ClientService {
         }
     }
 
-    private String findServer() {
+    private void findServers() {
         List<String> targets = lookup("TupleSpaces", null);
 
         while (targets == null || targets.isEmpty()) {
@@ -100,14 +105,27 @@ public class ClientService {
             }
             targets = lookup("TupleSpaces", null);
         }
-
-        return targets.get(0);
+        
+        for (String target : targets) {
+            createTupleSpacesStub(target);
+        }
     }
 
     public void put(String tuple) {
         try {
             debug("Sending put request with tuple " + tuple + "...");
-            tupleSpacesStub.put(PutRequest.newBuilder().setNewTuple(tuple).build());
+
+            for (int i = 0; i < numServers; i++) {
+                PutRequest request = PutRequest.newBuilder().setNewTuple(tuple).build();
+                tupleSpacesStubs.get(i).put(request, new ClientObserver<PutResponse>(collector));
+            }
+
+            try {
+                collector.waitForPutResponses(numServers);
+            } catch (InterruptedException e) {
+                System.out.println("Caught exception: " + e.getMessage());
+            }
+            
             System.out.println("OK\n");
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
@@ -118,8 +136,20 @@ public class ClientService {
     public void read(String searchPattern) {
         try {
             debug("Sending read request with search pattern " + searchPattern + "...");
-            ReadResponse result = tupleSpacesStub.read(ReadRequest.newBuilder().setSearchPattern(searchPattern).build());
-            System.out.println("OK\n" + result.getResult() + "\n");
+
+            for (int i = 0; i < numServers; i++) {
+                ReadRequest request = ReadRequest.newBuilder().setSearchPattern(searchPattern).build();
+                tupleSpacesStubs.get(i).read(request, new ClientObserver<ReadResponse>(collector));
+            }
+            
+             try {
+                collector.waitForReadResponse();
+            } catch (InterruptedException e) {
+                System.out.println("Caught exception: " + e.getMessage());
+            }
+            
+            System.out.println("OK\n" + collector.getReadTuple() + "\n");
+
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
             e.getStatus().getDescription());
@@ -129,8 +159,9 @@ public class ClientService {
     public void take(String searchPattern) {
         try {
             debug("Sending take request with search pattern " + searchPattern + "...");
-            TakeResponse result = tupleSpacesStub.take(TakeRequest.newBuilder().setSearchPattern(searchPattern).build());
-            System.out.println("OK\n" + result.getResult() + "\n");
+            // TODO: implement take
+            // TakeResponse result = tupleSpacesStub.take(TakeRequest.newBuilder().setSearchPattern(searchPattern).build());
+            // System.out.println("OK\n" + result.getResult() + "\n");
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
             e.getStatus().getDescription());
@@ -139,11 +170,12 @@ public class ClientService {
 
     public void getTupleSpacesState(int qualifier) {
         try {
-            getTupleSpacesStateResponse tuples = tupleSpacesStub.getTupleSpacesState(getTupleSpacesStateRequest.newBuilder().build());
-            System.out.println("OK");
+            // TODO: implement getTupleSpacesState
+            // getTupleSpacesStateResponse tuples = tupleSpacesStub.getTupleSpacesState(getTupleSpacesStateRequest.newBuilder().build());
+            // System.out.println("OK");
 
             // print in format [tuple1, tuple2, ...]
-            System.out.println("[" + String.join(", ", tuples.getTupleList()) + "]\n");
+            //System.out.println("[" + String.join(", ", tuples.getTupleList()) + "]\n");
 
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
@@ -154,7 +186,8 @@ public class ClientService {
     public void shutdown() {
         // Shutdown channel before stopping the process
         debug("Shutting down channel...");
-        channel.shutdownNow();
+        for (ManagedChannel channel : channels)  
+            channel.shutdownNow();
     }
 
 }
